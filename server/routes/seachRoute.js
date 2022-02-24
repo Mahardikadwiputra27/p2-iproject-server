e67a2f2e71e07c7b89bf7707eac870ea1c8cb82d
Mahardikadwiputra27 <mahardikadwiputra27@gmail.com>
2022-02-24 11:53:56 +0700

    fix

--- server/routes/seachRoute.js
+++ server/routes/seachRoute.js
@@ -2,9 +2,6 @@ const router = require('express').Router()
 
 
 const UniversityController = require('../controllers/universityController')
-
-// router.get('/country', UniversityController.findByCountry)
-// router.get('/name', UniversityController.findByName)
 router.get('/place', UniversityController.searchLocation)
 
 module.exports = router
